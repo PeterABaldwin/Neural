a73a9bf5312ef00d7601215e88e9f1a1a070be83
Kriltch <kriltch2@gmail.com>
2020-01-03 13:52:48 -0500

    Include may not work

--- NeuralCPP/neural-net.cpp
+++ NeuralCPP/neural-net.cpp
@@ -1,11 +1,15 @@
 //from a tutorial https://www.youtube.com/watch?v=KkwX7FkLfug
 
+#include <vector>
+
+using namespace std;
+
 class Net {
 public:
     Net(topology);
-    void feeForward(inputVals);
-    void backProp(targetVals);
-    void getResults(resultVals) const;
+    void feeForward(const vector<double> &inputVals);
+    void backProp(const vector<double> &targetVals);
+    void getResults(const vector<double> &resultVals) const;
 
 private:
 };
@@ -13,9 +17,12 @@ private:
 int main() {
     Net myNet(topology);
     
-
+    std::vector<double> inputVals;//std from C++ library (can remove if it feels like clutter)
     myNet.feeForward(inputVals);//training
+
+    vector<double> targetVals;
     myNet.backProp(targetVals);//what output should have been (backpropogation)
+
+    vector<double> resultVals;
     myNet.getResults(resultVals);//after trained
 }
-//a test
