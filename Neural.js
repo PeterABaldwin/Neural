63b8843b55a722414d43b87bf55e4a157a3a53cb
Kriltch <kriltch2@gmail.com>
2020-01-03 13:11:43 -0500

    Added Files

---
+++ Neural.js
@@ -0,0 +1,3 @@
+class Neural {
+    
+}
---
+++ NeuralCPP/neural-net.cpp
@@ -0,0 +1,20 @@
+//from a tutorial https://www.youtube.com/watch?v=KkwX7FkLfug
+
+class Net {
+public:
+    Net(topology);
+    void feeForward(inputVals);
+    void backProp(targetVals);
+    void getResults(resultVals) const;
+
+private:
+};
+
+int main() {
+    Net myNet(topology);
+    
+
+    myNet.feeForward(inputVals);//training
+    myNet.backProp(targetVals);//what output should have been (backpropogation)
+    myNet.getResults(resultVals);//after trained
+}
